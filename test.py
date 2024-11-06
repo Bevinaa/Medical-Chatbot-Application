af20baa866936bf0db3a37d3329d1cc53ea2de14
Bevinaa <144373337+Bevinaa@users.noreply.github.com>
2024-11-06 14:21:30 +0530

    Update  test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key ="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"
+openai.api_key ="your key"
 
 
 response = openai.ChatCompletion.create(
